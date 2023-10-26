15e47933f07e127517c608a8a3ca30d9e74a5b09
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-10-26 23:47:39 +0300

    refactor: delete variables

--- 05_telegram_bot_exchange_rates/helpers/constants.js
+++ 05_telegram_bot_exchange_rates/helpers/constants.js
@@ -1,6 +1,6 @@
-export const TG_BOT_TOKEN = '6735606227:AAFrNHw_4l0y2zzRDwxpz-3gsRmfY_YqpX8';
+export const TG_BOT_TOKEN = '';
 
-const WEATHER_API_KEY = '2289f5541cc09772e1304e763540c89d';
+const WEATHER_API_KEY = '';
 const CITY = 'Bachmut';
 export const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY}&units=metric&appid=${WEATHER_API_KEY}`;
 
